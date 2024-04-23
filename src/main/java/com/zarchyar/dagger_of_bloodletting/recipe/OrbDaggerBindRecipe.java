0a8f1bc5958f7d0317b8b90493489a581f85ae83
Zarchyar <72223492+Zarchyar@users.noreply.github.com>
2024-04-23 10:29:01 -0400

    Finally got the recipe to work

--- src/main/java/com/zarchyar/dagger_of_bloodletting/recipe/OrbDaggerBindRecipe.java
+++ src/main/java/com/zarchyar/dagger_of_bloodletting/recipe/OrbDaggerBindRecipe.java
@@ -4,10 +4,8 @@ import com.google.common.collect.Lists;
 import com.google.gson.JsonObject;
 import com.zarchyar.dagger_of_bloodletting.Dagger_Of_Bloodletting;
 import com.zarchyar.dagger_of_bloodletting.item.ModItems;
-import net.minecraft.core.NonNullList;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.IntTag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.GsonHelper;
@@ -21,14 +19,8 @@ import wayoftime.bloodmagic.common.item.BloodMagicItems;
 import java.util.List;
 
 public class OrbDaggerBindRecipe extends CustomRecipe {
-    private ItemStack output = new ItemStack(ModItems.DAGGEROFORB.get());
-    private final NonNullList<Ingredient> bloodOrbs = NonNullList.of(
-            Ingredient.of(BloodMagicItems.WEAK_BLOOD_ORB.get()),
-            Ingredient.of(BloodMagicItems.APPRENTICE_BLOOD_ORB.get()),
-            Ingredient.of(BloodMagicItems.MAGICIAN_BLOOD_ORB.get()),
-            Ingredient.of(BloodMagicItems.MASTER_BLOOD_ORB.get()),
-            Ingredient.of(BloodMagicItems.ARCHMAGE_BLOOD_ORB.get())
-    );
+    private Ingredient input = Ingredient.of(ModItems.DAGGEROFORB.get());
+    private final Ingredient orbIngredient = Ingredient.of(BloodMagicItems.WEAK_BLOOD_ORB.get(), BloodMagicItems.APPRENTICE_BLOOD_ORB.get(), BloodMagicItems.MAGICIAN_BLOOD_ORB.get(), BloodMagicItems.MASTER_BLOOD_ORB.get(), BloodMagicItems.ARCHMAGE_BLOOD_ORB.get());
 
     public OrbDaggerBindRecipe(ResourceLocation pId, CraftingBookCategory pCategory) {
         super(pId, pCategory);
@@ -46,7 +38,7 @@ public class OrbDaggerBindRecipe extends CustomRecipe {
                 list.add(itemStack);
                 if (list.size() > 2) {return false;}
                 if (list.size() > 1){
-                    if ((bloodOrbs.contains(Ingredient.of(list.get(0).getItem())) ^ bloodOrbs.contains(Ingredient.of(list.get(1).getItem()))) && (list.get(0).is(output.getItem()) ^ list.get(1).is(output.getItem()))){
+                    if ((orbIngredient.test(list.get(0)) ^ orbIngredient.test(list.get(1))) && ((input.test(list.get(0))) ^ (input.test(list.get(1))))){
                         return true;
                     }
                 }
@@ -57,39 +49,51 @@ public class OrbDaggerBindRecipe extends CustomRecipe {
 
     @Override
     public ItemStack assemble(CraftingContainer pContainer, RegistryAccess pRegistryAccess) {
-        ItemStack orbItem = null;
+        ItemStack orbItem = new ItemStack(BloodMagicItems.WEAK_BLOOD_ORB.get());
+        ItemStack swordItem = new ItemStack(ModItems.DAGGEROFORB.get());
         Integer i = 0;
 
         for (int j = 0; j < pContainer.getContainerSize(); j++) {
             ItemStack itemStack = pContainer.getItem(j);
             if (!itemStack.isEmpty()) {
                 i++;
-                if (bloodOrbs.contains(Ingredient.of(itemStack.getItem()))){
+                if (orbIngredient.test(itemStack)){
                     orbItem = itemStack;
                 }
-                if (itemStack.is(output.getItem())){
-                    this.output = itemStack.copy();
+                if (input.test(itemStack)){
+                    swordItem = itemStack.copy();
                 }
             }
             if (i == 2){
-                if (bloodOrbs.get(0).test(orbItem)){
-                    this.output.setTag((CompoundTag) this.output.getTag().copy().put("boundorb", IntTag.valueOf(0)));
+                CompoundTag swordTags = swordItem.getTag().copy();
+                if (Ingredient.of(BloodMagicItems.WEAK_BLOOD_ORB.get()).test(orbItem)){
+                    swordTags.putInt("boundorb", 0);
+                    swordItem.setTag(swordTags);
+                    return swordItem;
                 }
-                if (bloodOrbs.get(1).test(orbItem)){
-                    this.output.setTag((CompoundTag) this.output.getTag().copy().put("boundorb", IntTag.valueOf(1)));
+                if (Ingredient.of(BloodMagicItems.APPRENTICE_BLOOD_ORB.get()).test(orbItem)){
+                    swordTags.putInt("boundorb", 1);
+                    swordItem.setTag(swordTags);
+                    return swordItem;
                 }
-                if (bloodOrbs.get(2).test(orbItem)){
-                    this.output.setTag((CompoundTag) this.output.getTag().copy().put("boundorb", IntTag.valueOf(2)));
+                if (Ingredient.of(BloodMagicItems.MAGICIAN_BLOOD_ORB.get()).test(orbItem)){
+                    swordTags.putInt("boundorb", 2);
+                    swordItem.setTag(swordTags);
+                    return swordItem;
                 }
-                if (bloodOrbs.get(3).test(orbItem)){
-                    this.output.setTag((CompoundTag) this.output.getTag().copy().put("boundorb", IntTag.valueOf(3)));
+                if (Ingredient.of(BloodMagicItems.MASTER_BLOOD_ORB.get()).test(orbItem)){
+                    swordTags.putInt("boundorb", 3);
+                    swordItem.setTag(swordTags);
+                    return swordItem;
                 }
-                if (bloodOrbs.get(4).test(orbItem)){
-                    this.output.setTag((CompoundTag) this.output.getTag().copy().put("boundorb", IntTag.valueOf(4)));
+                if (Ingredient.of(BloodMagicItems.ARCHMAGE_BLOOD_ORB.get()).test(orbItem)){
+                    swordTags.putInt("boundorb", 4);
+                    swordItem.setTag(swordTags);
+                    return swordItem;
                 }
             }
         }
-        return this.output;
+        return swordItem;
     }
 
     @Override
